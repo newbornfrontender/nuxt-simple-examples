064e7ca5609ff2cbf0d252e66235b7ba5959b907
Infant Frontender <newbornfrontender@gmail.com>
2018-09-17 14:57:13 +0300

    Настроил компонент списка

---
+++ components/ItemsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li
+      v-for="item of itemsList"
+      v-bind:key="item.id"
+    >
+      <nuxt-link v-bind:to="item.path">{{ item.title }}</nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    props: {
+      itemsList: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+</script>
