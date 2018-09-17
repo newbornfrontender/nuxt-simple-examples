c9d19a232da3e5933c776dc082c9d8e38ba30257
Infant Frontender <newbornfrontender@gmail.com>
2018-09-17 14:52:21 +0300

    Сделал модуль @nuxtjs/pwa отдельным

---
+++ modules/pwa.js
@@ -0,0 +1,15 @@
+module.exports = ['@nuxtjs/pwa', {
+  meta: {
+    name: 'Простое приложение',
+    author: 'Infant Frontender',
+    description: false,
+    ogType: false,
+    ogSiteName: false,
+    ogTitle: false,
+  },
+  manifest: {
+    name: 'Простое приложение',
+    short_name: 'Приложение',
+    lang: 'ru',
+  },
+}];
