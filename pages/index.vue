b16452490cd56c0e5a18c5e3c8846de7d2bf1889
Infant Frontender <newbornfrontender@gmail.com>
2018-09-17 12:16:14 +0300

    Добавил главную страницу с мета-информацией

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      msg: 'Hello world!',
+    }),
+
+    head () {
+      return {
+        title: 'Главная',
+        meta: [{
+          hid: 'index-description',
+          name: 'description',
+          content: 'Описание главной страницы',
+        }],
+      };
+    },
+  };
+</script>
