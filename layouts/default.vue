9ed5b3fa04877c4f477bd7831bd9567fa7994cdd
Infant Frontender <newbornfrontender@gmail.com>
2018-09-17 14:54:13 +0300

    Настроил базовый шаблон

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+    <main-page-header />
+
+    <main-page-content />
+
+    <main-page-footer />
+  </section>
+</template>
+
+<script>
+  import MainPageHeader from '~/components/MainPageHeader';
+  import MainPageContent from '~/components/MainPageContent';
+  import MainPageFooter from '~/components/MainPageFooter';
+
+  export default {
+    components: {
+      MainPageHeader,
+      MainPageContent,
+      MainPageFooter,
+    },
+  };
+</script>
