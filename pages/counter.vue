13b3f5d356595d95e3a6e2e24e652c14520bfc3d
Infant Frontender <newbornfrontender@gmail.com>
2018-09-17 14:53:39 +0300

    Настроил страницу счетчика

---
+++ pages/counter.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h1>{{ title }}</h1>
+  </section>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      title: 'Счетчик',
+    }),
+
+    head () {
+      return {
+        title: 'Счетчик',
+        meta: [{
+          hid: 'counter-description',
+          name: 'description',
+          content: 'Описание страницы счетчика',
+        }],
+      };
+    },
+  };
+</script>
