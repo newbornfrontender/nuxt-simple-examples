afede77e612fbcc43b4fd69fa131ff7c49d61bf1
Infant Frontender <newbornfrontender@gmail.com>
2018-09-17 14:54:29 +0300

    Настроил шаблон ошибки

---
+++ layouts/error.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <h1 v-if="checkStatusCode(404)">{{ title }}</h1>
+    <h1 v-else>Ошибка в коде</h1>
+
+    <nuxt-link to="/">Перейти на главную страницу</nuxt-link>
+  </section>
+</template>
+
+<script>
+  export default {
+    props: {
+      error: {
+        type: Object,
+      },
+    },
+
+    data: () => ({
+      title: 'Страница не найдена',
+    }),
+
+    methods: {
+      checkStatusCode (num) {
+        return this.error.statusCode === num;
+      },
+    },
+
+    head () {
+      return {
+        title: this.title,
+        meta: [{
+          hid: 'error-description',
+          name: 'description',
+          content: 'Описание страницы с ошибкой',
+        }],
+      };
+    },
+  };
+</script>
