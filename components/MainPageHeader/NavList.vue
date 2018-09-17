7006493d93ef616074e408e9364ab809be7e20a2
Infant Frontender <newbornfrontender@gmail.com>
2018-09-17 14:56:42 +0300

    Настроил компонент навигационного списка

---
+++ components/MainPageHeader/NavList.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <items-list v-bind:items-list="navList" />
+  </nav>
+</template>
+
+<script>
+  import ItemsList from '~/components/ItemsList';
+
+  export default {
+    data: () => ({
+      navList: [{
+        title: 'Главная',
+        path: '/',
+      }, {
+        title: 'Счетчик',
+        path: '/counter',
+      }],
+    }),
+
+    components: {
+      ItemsList,
+    },
+  };
+</script>
