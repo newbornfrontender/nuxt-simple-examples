50909c8dc18c8a0fca8243ea95fcdf6a69e6ab25
Infant Frontender <newbornfrontender@gmail.com>
2018-09-17 14:56:13 +0300

    Настроил компонент шапки

---
+++ components/MainPageHeader/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <nav-list />
+  </header>
+</template>
+
+<script>
+  import NavList from './NavList';
+
+  export default {
+    components: {
+      NavList,
+    },
+  };
+</script>
